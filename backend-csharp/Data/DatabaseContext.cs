0f1f408503eada255532138473bc5898c0833894
Nicholas Riegel <nicholaspriegel@gmail.com>
2024-06-20 15:21:20 -0400

    added commenting to backend

--- backend-csharp/Data/DatabaseContext.cs
+++ backend-csharp/Data/DatabaseContext.cs
@@ -5,12 +5,18 @@ namespace backend_csharp.Data
 {
     public class DatabaseContext
     {
+        // connection string property
         private readonly string _connectionString;
 
+        // constructor (identified by having same name as class)
         public DatabaseContext(string connectionString)
         {
             _connectionString = connectionString;
         }
+
+        // This the the method that will be used to create a connection to the database
+        // Its type is "IDbConnection," which is an "interface" that NpgsqlConnection implements
+        // An interface is a set of rules that a class must follow
 
         public IDbConnection CreateConnection() => new NpgsqlConnection(_connectionString);
     }
--- backend-csharp/Program.cs
+++ backend-csharp/Program.cs
@@ -1,14 +1,31 @@
+/* ==========================================================================
+For all of this code I relied pretty heavily on interactions with ChatGPT. 
+However, I did not accept all of its suggestions.
+In particular, I rejected its responses where I could not see the logic behind the suggestion.
+And I tried to understand every line of code that I used from ChatGPT.
+ ==========================================================================*/
+
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+// call in the DatabaseContext class
 using backend_csharp.Data;
+// Dapper is an "ORM" that allows you to map data from a database to a C# object
+// An ORM is an Object-Relational Mapping tool that simplifies the process of mapping data between objects in code and tables in a database
 using Dapper;
+// call in the Todo model
 using backend_csharp.Models;
 using System.Data;
 
+
+
+
+// Create a new WebApplication instance
+// WebApplication is a class that is part of the ASP.NET Core framework (Microsoft.AspNetCore.Builder)
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = "Host=localhost;Port=5432;Database=csharp_webapi_01;Username=ubuntu;Password=ubuntu;";
+// A "singleton" is a class that is instantiated only once
 builder.Services.AddSingleton(new DatabaseContext(connectionString));
 
 builder.Services.AddCors();
@@ -22,24 +39,36 @@ app.UseCors(builder =>
            .AllowAnyHeader();
 });
 
+// If the app is not in development mode, use HTTPS
 if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
 
 // Get all todos
+// dbContext is a new instance of the class \
+// the connection is already in the app
 app.MapGet("/todos", async (DatabaseContext dbContext) =>
 {
+    // CreateConnection is a method in the DatabaseContext class
     using var connection = dbContext.CreateConnection();
+    // QueryAsync is a method in the Dapper that is added to IDbConnection (set in the DatabaseContext class)
+    // Dapper's QueryAsync<T> method is a generic method. It means that T can be any type. It is the return type: what is returned from the database
+    // Dapper Query commands are just for getting data; Execute commands are for inserting, updating, or deleting data
     var todos = await connection.QueryAsync<Todo>("SELECT * FROM todos ORDER BY id;");
+    // returns a 200 ok along with the todos
     return Results.Ok(todos);
 });
 
 // Post a new todo
+// newTodo is what is passed in the body of the post request
 app.MapPost("/todos", async (Todo newTodo, DatabaseContext dbContext) =>
 {
     using var connection = dbContext.CreateConnection();
     var sql = "INSERT INTO todos (description, completed) VALUES (@Description, @Completed) RETURNING id;";
+    // ExecuteScalarAsync's return type is int because it is returning the id of the new todo which will be added to the new todo and sent back to the client
+    // new {} is an anonymous object that is used to pass parameters to the query. Dapper just requires that it be done this way.
+    // Dapper Execute commands are for inserting, updating, or deleting data; Query commands are just for getting data
     var newId = await connection.ExecuteScalarAsync<int>(sql, new { newTodo.Description, newTodo.Completed });
 
     if (newId > 0)
@@ -51,19 +80,21 @@ app.MapPost("/todos", async (Todo newTodo, DatabaseContext dbContext) =>
     return Results.Problem("An error occurred while creating the todo.");
 });
 
-// Get a single todo
-app.MapGet("/todos/todo/{id}", async (int id, DatabaseContext dbContext) =>
-{
-    using var connection = dbContext.CreateConnection();
-    var todo = await connection.QueryFirstOrDefaultAsync<Todo>("SELECT * FROM todos WHERE Id = @Id;", new { Id = id });
-    return todo is not null ? Results.Ok(todo) : Results.NotFound();
-});
+// Get a single todo (Not used at the moment)
+// app.MapGet("/todos/todo/{id}", async (int id, DatabaseContext dbContext) =>
+// {
+//     using var connection = dbContext.CreateConnection();
+//     var todo = await connection.QueryFirstOrDefaultAsync<Todo>("SELECT * FROM todos WHERE Id = @Id;", new { Id = id });
+//     return todo is not null ? Results.Ok(todo) : Results.NotFound();
+// });
 
 // Update a todo
+// id and updatedTodo are passed in the body of the put request
 app.MapPut("/todos/todo/{id}", async (int id, Todo updatedTodo, DatabaseContext dbContext) =>
 {
     using var connection = dbContext.CreateConnection();
     var sqlUpdate = "UPDATE todos SET description = @Description, completed = @Completed WHERE id = @Id;";
+    // Dapper Execute commands are for inserting, updating, or deleting data; Query commands are just for getting data
     var result = await connection.ExecuteAsync(sqlUpdate, new { updatedTodo.Description, updatedTodo.Completed, Id = id });
 
     if (result > 0)
